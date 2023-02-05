3670d1310e670cd1788b59ba686df3f1d4a11b34
vzotikas <vzotikas@vzotikas.com>
2023-02-05 15:10:17 +0100

    Initial Commit

---
+++ Coordinator/CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorApp.swift
+//  Coordinator
+//
+//  Created by Administrator on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
